e25264825a08e4f0c0e9b8da9ef6c7666bd3ad5e
Zafiadomgit <davidpietersjuegos@gmail.com>
2025-08-22 11:34:19 -0500

    se arregla error en vercel

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { body } from 'express-validator';
-import { auth } from '../middleware/auth.js';
+import { authMiddleware as auth } from '../middleware/auth.js';
 import {
   crearReview,
   obtenerReviewsProfesor,
